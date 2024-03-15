031de00d21695e9d50165f9747253a3b5284faaf
ashiouzzaman <ashiouzzaman.code@gmail.com>
2024-03-15 15:25:07 +0600

    Task 4 done

---
+++ task4.js
@@ -0,0 +1,12 @@
+let name = 'Real'
+let age = 20
+let examList = ['JSC', 'PSC', 'SSC', 'HSC', 'BSC']
+
+let result = Array.isArray(name)
+result? console.log('Name is an Array!'): console.log('Name is not an Array!')
+
+result = Array.isArray(age)
+result? console.log('Age is an Array!'): console.log('Age is not an Array!')
+
+result = Array.isArray(examList)
+result? console.log('Exam is an Array!'): console.log('Exam is not an Array!')
